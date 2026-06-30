183a4c5b14cb9feb760c40ee60b69dcd8d30c08c
shaig-mahmudov <shaigfm-apa101@code.edu.az>
2026-06-30 11:29:25 +0400

    feat: initialize spring boot application entry point and basic test

---
+++ src/test/java/com/loopin/api/LoopinApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loopin.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoopinApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
