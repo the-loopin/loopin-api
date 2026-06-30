be34c940e52125f8b85249fa9506d0e3c5c81383
shaig-mahmudov <zyphorix.official@gmail.com>
2026-06-30 04:16:49 -0700

    feat: update BaseEntity parent class

--- src/main/java/com/loopin/api/entity/common/entity/BaseEntity.java
+++ src/main/java/com/loopin/api/entity/common/entity/BaseEntity.java
@@ -1,16 +1,21 @@
-package com.loopin.api.entity.common;
+package com.loopin.api.entity.common.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
-import org.springframework.data.annotation.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 
-
 @MappedSuperclass
-public class BaseEntity {
+@Getter
+@Setter
+public abstract class BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -21,7 +26,19 @@ public class BaseEntity {
 
     @Column(nullable = false)
     private LocalDateTime updatedAt;
+
     private LocalDateTime deletedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
 
     public void markAsDeleted() {
         this.deletedAt = LocalDateTime.now();
