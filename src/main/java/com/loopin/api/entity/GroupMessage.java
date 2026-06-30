fb33c7d7455a4c355768d4fc7f4435a5447c853c
shaig-mahmudov <zyphorix.official@gmail.com>
2026-06-30 04:19:01 -0700

    feat: add GroupMessage entity

---
+++ src/main/java/com/loopin/api/entity/GroupMessage.java
@@ -0,0 +1,25 @@
+package com.loopin.api.entity;
+
+import com.loopin.api.entity.common.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "group_messages")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class GroupMessage extends BaseEntity {
+
+    @ManyToOne
+    @JoinColumn(name = "group_id", nullable = false)
+    private EventGroup group;
+
+    @ManyToOne
+    @JoinColumn(name = "sender_id", nullable = false)
+    private User sender;
+
+    @Column(nullable = false)
+    private String messageText;
+}
