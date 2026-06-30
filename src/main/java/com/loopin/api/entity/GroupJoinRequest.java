cda84b66158007e315fae9c345217acbe750ec43
shaig-mahmudov <zyphorix.official@gmail.com>
2026-06-30 04:18:47 -0700

    feat: add GroupJoinRequest entity

---
+++ src/main/java/com/loopin/api/entity/GroupJoinRequest.java
@@ -0,0 +1,28 @@
+package com.loopin.api.entity;
+
+import com.loopin.api.entity.common.entity.BaseEntity;
+import com.loopin.api.entity.common.enums.RequestStatus;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "group_join_requests")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class GroupJoinRequest extends BaseEntity {
+
+    @ManyToOne
+    @JoinColumn(name = "group_id", nullable = false)
+    private EventGroup group;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Enumerated(EnumType.STRING)
+    private RequestStatus status = RequestStatus.PENDING;
+
+    private String message;
+}
