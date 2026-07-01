9f155c450d4bd10e4adff5910b25911876dabc3a
URLeeo <mammadzadaaslan05@gmail.com>
2026-07-01 13:48:30 +0400

    feat: add EventCategory, EventStatus, EventType enums

---
+++ src/main/java/com/loopin/api/entity/common/enums/EventCategory.java
@@ -0,0 +1,14 @@
+package com.loopin.api.entity.common.enums;
+
+public enum EventCategory {
+    TECH,
+    STARTUP,
+    HR,
+    EDUCATION,
+    TRAVEL,
+    SPORT,
+    SOCIAL,
+    LANGUAGE,
+    CREATIVE,
+    OTHER
+}
