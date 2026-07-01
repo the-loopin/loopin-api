33f372803d03d2825ba2fb629434c286693a77b5
URLeeo <mammadzadaaslan05@gmail.com>
2026-07-01 15:31:12 +0400

    feat: add Event dtos

---
+++ src/main/java/com/loopin/api/dto/event/response/EventResponse.java
@@ -0,0 +1,34 @@
+package com.loopin.api.dto.event.response;
+
+import com.loopin.api.entity.common.enums.EventCategory;
+import com.loopin.api.entity.common.enums.EventStatus;
+import com.loopin.api.entity.common.enums.EventType;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class EventResponse {
+
+    private Long id;
+    private String title;
+    private String description;
+    private EventType type;
+    private EventCategory category;
+    private String city;
+    private String address;
+    private LocalDateTime startDateTime;
+    private LocalDateTime endDateTime;
+    private Boolean isFree;
+    private BigDecimal price;
+    private String organizerName;
+    private String imageUrl;
+    private EventStatus status;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}
