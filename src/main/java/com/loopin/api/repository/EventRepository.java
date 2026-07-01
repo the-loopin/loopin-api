70e68cfed80802882f242c111c2e10520c71f111
URLeeo <mammadzadaaslan05@gmail.com>
2026-07-01 13:48:39 +0400

    feat: add EventRepository

---
+++ src/main/java/com/loopin/api/repository/EventRepository.java
@@ -0,0 +1,8 @@
+package com.loopin.api.repository;
+
+import com.loopin.api.entity.Event;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+public interface EventRepository extends JpaRepository<Event, Long>, JpaSpecificationExecutor<Event> {
+}
