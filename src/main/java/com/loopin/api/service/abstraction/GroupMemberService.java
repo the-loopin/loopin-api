a31163a7e1803e1da933e0078c6f9d6f37a12415
Ulvi Haciyev <ulvihaciyev551@gmail.com>
2026-07-02 21:12:02 +0400

    feature: GroupMemberService added

---
+++ src/main/java/com/loopin/api/service/abstraction/GroupMemberService.java
@@ -0,0 +1,15 @@
+package com.loopin.api.service.abstraction;
+
+
+import com.loopin.api.dto.group.request.GroupMemberRequest;
+import com.loopin.api.dto.group.response.GroupMemberResponse;
+import com.loopin.api.entity.GroupMember;
+
+import java.util.List;
+
+public interface GroupMemberService {
+    public GroupMemberResponse getById(Long id);
+    public List<GroupMemberResponse> getByGroupId(Long groupId);
+    public void removeMember(Long groupId, Long userId);
+    public GroupMemberResponse addMember(GroupMemberRequest dto);
+}
