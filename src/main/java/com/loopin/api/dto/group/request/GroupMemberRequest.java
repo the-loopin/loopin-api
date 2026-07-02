df2db431468481c98fad4fce4995cc528b186832
Ulvi Haciyev <ulvihaciyev551@gmail.com>
2026-07-02 21:09:29 +0400

    feature: GroupMemberRequest added

---
+++ src/main/java/com/loopin/api/dto/group/request/GroupMemberRequest.java
@@ -0,0 +1,15 @@
+package com.loopin.api.dto.group.request;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class GroupMemberRequest {
+    @NotNull
+    private Long groupId;
+
+    @NotNull
+    private Long userId;
+}
