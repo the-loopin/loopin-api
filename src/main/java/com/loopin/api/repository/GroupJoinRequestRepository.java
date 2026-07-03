991af0dfe6c055efc45477345c6a8ca6735c6e47
Ulvi Haciyev <ulvihaciyev551@gmail.com>
2026-07-03 13:25:47 +0400

    feature: GroupJoin Repo added

---
+++ src/main/java/com/loopin/api/repository/GroupJoinRequestRepository.java
@@ -0,0 +1,22 @@
+package com.loopin.api.repository;
+
+
+import com.loopin.api.common.enums.RequestStatus;
+import com.loopin.api.entity.GroupJoinRequest;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface GroupJoinRequestRepository extends JpaRepository<GroupJoinRequest, Long> {
+
+    List<GroupJoinRequest> findByGroupId(Long groupId);
+
+    List<GroupJoinRequest> findByUserId(Long userId);
+
+    List<GroupJoinRequest> findByGroupIdAndStatus(Long groupId, RequestStatus status);
+
+    Optional<GroupJoinRequest> findByGroupIdAndUserId(Long groupId, Long userId);
+
+    boolean existsByGroupIdAndUserIdAndStatus(Long groupId, Long userId, RequestStatus status);
+}
