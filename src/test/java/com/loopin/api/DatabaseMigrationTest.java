fd47605361c3f05323286064963b6f7f73d3c001
shaig-mahmudov <shaigfm-apa101@code.edu.az>
2026-07-05 17:20:21 +0400

    test(db): update database migration test tables list with user_reports

--- src/test/java/com/loopin/api/DatabaseMigrationTest.java
+++ src/test/java/com/loopin/api/DatabaseMigrationTest.java
@@ -39,6 +39,7 @@ class DatabaseMigrationTest {
                         "group_members",
                         "group_join_requests",
                         "group_messages",
+                        "user_reports",
                         "databasechangelog"
                 );
     }
