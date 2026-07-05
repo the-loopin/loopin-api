93fc2c7f2aba26e244f8e6f3f200aedd054c542e
shaig-mahmudov <shaigfm-apa101@code.edu.az>
2026-07-05 19:05:58 +0400

    refactor: update GroupMemberRequest DTO with public UUID field

--- src/main/java/com/loopin/api/dto/group/request/GroupMemberRequest.java
+++ src/main/java/com/loopin/api/dto/group/request/GroupMemberRequest.java
@@ -4,10 +4,12 @@ import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.UUID;
+
 @Getter
 @Setter
 public class GroupMemberRequest {
 
     @NotNull
-    private Long userId;
+    private UUID userId;
 }
