ea84c397faa6206f5b461a27a5c38fdc65e7a9ad
URLeeo <mammadzadaaslan05@gmail.com>
2026-07-07 21:11:32 +0400

    Add active group lookup by event

--- src/main/java/com/loopin/api/repository/EventGroupRepository.java
+++ src/main/java/com/loopin/api/repository/EventGroupRepository.java
@@ -1,12 +1,16 @@
 package com.loopin.api.repository;
 
 
+import com.loopin.api.common.enums.GroupStatus;
 import com.loopin.api.entity.EventGroup;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Collection;
 import java.util.List;
 
 public interface EventGroupRepository extends JpaRepository<EventGroup, Long> {
 
     List<EventGroup> findByEventId(Long eventId);
+
+    List<EventGroup> findByEventIdAndStatusIn(Long eventId, Collection<GroupStatus> statuses);
 }
