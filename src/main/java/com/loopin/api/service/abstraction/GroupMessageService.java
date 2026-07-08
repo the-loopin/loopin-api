80083d45e4ab3770a6ad2c35f790134fbbe254bb
shaig-mahmudov <shaigfm-apa101@code.edu.az>
2026-07-08 11:56:49 +0400

    refactor: update GroupMessageService interface to use UUIDs and pagination

--- src/main/java/com/loopin/api/service/abstraction/GroupMessageService.java
+++ src/main/java/com/loopin/api/service/abstraction/GroupMessageService.java
@@ -3,11 +3,14 @@ package com.loopin.api.service.abstraction;
 import com.loopin.api.dto.message.request.CreateGroupMessageRequest;
 import com.loopin.api.dto.message.response.GroupMessageResponse;
 
-import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.UUID;
 
 public interface GroupMessageService {
 
-    List<GroupMessageResponse> getGroupMessages(Long groupId, Long currentUserId);
+    Page<GroupMessageResponse> getGroupMessages(UUID groupId, Long currentUserId, Pageable pageable);
 
-    GroupMessageResponse sendMessage(Long groupId, Long currentUserId, CreateGroupMessageRequest request);
+    GroupMessageResponse sendMessage(UUID groupId, Long currentUserId, CreateGroupMessageRequest request);
 }
