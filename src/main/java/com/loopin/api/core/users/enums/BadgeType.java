58175278737e6cb85889d104f815a466ae768847
shaig-mahmudov <shaigfm-apa101@code.edu.az>
2026-07-10 03:42:06 +0400

    refactor(users): clean up deprecated core files from users module

--- src/main/java/com/loopin/api/core/users/enums/BadgeType.java
+++
@@ -1,6 +0,0 @@
-package com.loopin.api.core.users.enums;
-
-public enum BadgeType {
-    EVENT_ATTENDEE,
-    GROUP_CREATOR
-}
--- src/main/java/com/loopin/api/core/users/service/UserPresenceService.java
+++
@@ -1,6 +0,0 @@
-package com.loopin.api.core.users.service;
-
-public interface UserPresenceService {
-    void handleUserConnect(Long userId);
-    void handleUserDisconnect(Long userId);
-}
