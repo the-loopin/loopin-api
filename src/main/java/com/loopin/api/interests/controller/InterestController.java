9165b122472e3c7d83f6ea640dfbd8567f87ba91
shaig-mahmudov <shaigfm-apa101@code.edu.az>
2026-07-10 03:42:02 +0400

    feat(interests): add support utilities and small classes for interests module

---
+++ src/main/java/com/loopin/api/interests/controller/InterestController.java
@@ -0,0 +1,24 @@
+package com.loopin.api.core.interests.controller;
+
+import com.loopin.api.core.interests.dto.InterestResponse;
+import com.loopin.api.core.interests.service.InterestService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/interests")
+@RequiredArgsConstructor
+public class InterestController {
+
+    private final InterestService interestService;
+
+    @GetMapping
+    public ResponseEntity<List<InterestResponse>> getInterests() {
+        return ResponseEntity.ok(interestService.getInterests());
+    }
+}
