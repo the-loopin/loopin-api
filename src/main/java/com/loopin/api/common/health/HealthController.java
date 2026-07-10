2f745e395b5e35d3e4db8c0812180ba16d2c8a9c
shaig-mahmudov <zyphorix.official@gmail.com>
2026-07-10 04:17:17 -0700

    feat(api): version HealthController endpoint to /v1

--- src/main/java/com/loopin/api/common/health/HealthController.java
+++ src/main/java/com/loopin/api/common/health/HealthController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/health")
+@RequestMapping("/v1/health")
 public class HealthController {
 
     @GetMapping
