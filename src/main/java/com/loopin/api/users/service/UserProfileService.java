ed3a7c8daed954d127aaf2fe98d11087d810a08d
Shaig Mahmudov <mahmudovshaig@gmail.com>
2026-07-14 03:54:47 +0400

    feat(users): add avatar service operations

--- src/main/java/com/loopin/api/users/service/UserProfileService.java
+++ src/main/java/com/loopin/api/users/service/UserProfileService.java
@@ -2,18 +2,32 @@ package com.loopin.api.users.service;
 
 import com.loopin.api.interests.dto.InterestResponse;
 import com.loopin.api.interests.dto.UpdateUserInterestsRequest;
-import com.loopin.api.users.dto.profile.response.UserProfileResponse;
 import com.loopin.api.users.dto.profile.request.UpdateUserProfileRequest;
+import com.loopin.api.users.dto.profile.response.UserProfileResponse;
 
 import java.util.List;
+import java.util.UUID;
 
 public interface UserProfileService {
 
     UserProfileResponse getProfile(Long userId);
 
-    UserProfileResponse updateProfile(Long userId, UpdateUserProfileRequest request);
+    UserProfileResponse updateProfile(
+        Long userId,
+        UpdateUserProfileRequest request
+    );
 
-    List<InterestResponse> updateInterests(Long userId, UpdateUserInterestsRequest request);
+    UserProfileResponse updateAvatar(
+        Long userId,
+        UUID mediaId
+    );
+
+    UserProfileResponse removeAvatar(Long userId);
+
+    List<InterestResponse> updateInterests(
+        Long userId,
+        UpdateUserInterestsRequest request
+    );
 
     List<InterestResponse> getInterests(Long userId);
 
